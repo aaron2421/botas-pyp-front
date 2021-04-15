a5658cb93f37b38f00a151ebf380a9f8e4c43f54
Aaron <aaronariasramos@gmail.com>
2021-04-14 19:08:10 -0600

    validacion temporal de login

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  username: string = "";
+  pswd: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setUsername(event: Event){
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  setPswd(event: Event){
+    this.pswd = (<HTMLInputElement>event.target).value;
+  }
+
+  iniciarSesion(){
+    if (this.username === 'Aaron' && this.pswd === '12345') {
+      alert('sesion iniciada');
+    } else if (this.username === ""  && this.pswd === ""){
+      alert('los campos no pueden estar vacios');
+    } else {
+      alert('Te equivocaste');
+    }
   }
 
 }
